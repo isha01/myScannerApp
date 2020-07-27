adf1ce7a325d4aa3e18a6a79c82b005bcca01566
ijindal <ijindal@expediagroup.com>
2020-07-27 21:46:26 +0530

    First commit

---
+++ src/main/java/com/example/myScannerApp/MyScannerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myScannerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScannerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScannerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myScannerApp/MyScannerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myScannerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScannerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
